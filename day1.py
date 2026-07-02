8d1978895acf860f2c9f23606b4820b1d58b84a2
Mahalakshmi <ssmaha2002@gmail.com>
2026-07-02 13:34:39 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,19 @@
+#program 1
+name = "maha"
+print(f"Hello, my name is{name}")
+#program 2
+name = "maha"
+age = 20
+cgpa = 8.1
+print(f"{name} is {age} years old and cgpa is {cgpa}")
+#program 3
+print(type(name))
+print(type(age))
+print(type(cgpa))
+#program 4
+a = "25"
+num =int(a)
+print(num + 5)
+#program 5
+a = "machine learning"
+print(a[0:7])
